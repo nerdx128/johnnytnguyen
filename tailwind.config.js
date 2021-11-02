e21ffa631ee35534c5f608cb4505e6e6174f5828
Johnny Nguyen <johnny.t.nguyen.128@gmail.com>
2021-11-02 15:49:48 -0500

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['LemonMilk', 'Helvetica', 'Arial', 'sans-serif']
+      },
+      zIndex: {
+        '1': '1',
+        '60': '60',
+        '70': '70',
+        '80': '80',
+        '90': '90',
+        '100': '100',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
